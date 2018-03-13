9d2bee5e9106b1befc166889e641dfde88117728
Joseph Trattner <joe@Josephs-MacBook-Pro.local>
2018-03-13 12:23:03 -0500

    Added an eception handler for a failed delete

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.exception;
 
-import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import java.util.Date;
+
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserDeleteFailedException;
+import com.in28minutes.rest.webservices.restfulwebservices.user.UserNotFoundException;
 
 // This is a Restcontroller because it will provide a response back.  We want this to be used accross all controllers
 // so we give it the @ControllerAdvice annotation.  We are overriding what is being done for all exceptions in the
@@ -22,14 +25,20 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 
     // we do a new ExceptionResponse with is a custom class we created and we pass in the timestamp,
     // message and message details as defined in that customized class
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
-        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+//        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+//        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
     @ExceptionHandler(UserNotFoundException.class)
     public final ResponseEntity<Object> handleUserNotFoundException(UserNotFoundException ex, WebRequest request) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+    
+    @ExceptionHandler(UserDeleteFailedException.class)
+    public final ResponseEntity<Object> handleUserDeleteFailedException(UserDeleteFailedException ex, WebRequest request) {
         ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -1,13 +1,20 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -42,10 +49,16 @@ public class UserJPAResource {
 
     @DeleteMapping("jpa/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        userRepository.delete(id);
-
+    	
+	    	try {
+	        userRepository.delete(id);
+	    
+	    // I added this to catch a delete that does not exist    
+	    	} catch (EmptyResultDataAccessException e) {
+	            throw new UserDeleteFailedException("Delete failed for id-"+id);
+	    	}        
     }
-
+    
     // the location gets returned in the response header
     @PostMapping("jpa/users")
     public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
